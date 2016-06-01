555a319a6e8e252389309e9aea189bbc25855d73
JackMasterson <jackmasterson5@gmail.com>
2016-06-01 00:34:39 -0400

    Pic animations so the two disapper and the 'both' appears, and ceremony info though not correctly laid out

--- js/script.js
+++ js/script.js
@@ -4,25 +4,52 @@ var model = {
 	mainSect: [
 		{
 			header: 'Beth Masterson and Andrew Knapp',
-			backImg: 'url(..//Beth/img/parchment.jpg)'
+			backImg: 'url(..//Beth/img/flowers.jpeg)'
 		},
 		{
 			header: 'Ceremony and Reception Details',
-			backImg: 'url(..//Beth/img/flowers.jpeg)'
+			backImg: 'url(..//Beth/img/bride.jpg)'
 		},
 		{
 
 			header: 'Hotel Information',
 			backImg: 'url(..//Beth/img/hotel.jpeg)'
 		}	
+	],
+
+	ceremony: [
+		{
+			header: "Ceremony",
+			subHead: "St. Mark's Church",
+			location: "Sea Girt, NJ 08750",
+			url: "google.com/maps"
+		},
+		{
+			header: "Reception",
+			subHead: "Spring Lake Bath & Tennis Club",
+			location: "Spring Lake, NJ 07762",
+			url: "google.com/maps"
+		}
 	]
 };
 
 var viewModel = {
 	init: function() {
-
+		picSwap();
 	}
 };
+
+		function picSwap () {
+		  $( ".left" ).animate({
+		    opacity: 0.75,
+		    height: "toggle"
+		  }, 1400, function() {
+		  	$(".left").fadeOut(1000);
+		  	$(".right").fadeOut(1000);
+		  	$(".both").fadeIn(1200); 
+		  })
+
+		};
 
 ko.applyBindings(viewModel.init());
 
