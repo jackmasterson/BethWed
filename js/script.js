a658e5abb6d1d1c77fc8b7cc19f40493f1931049
JackMasterson <jackmasterson5@gmail.com>
2016-06-01 22:57:15 -0400

    Add background slider 'chocolate' and updated intro-div

--- js/script.js
+++ js/script.js
@@ -3,15 +3,17 @@
 var model = {
 	mainSect: [
 		{
+			sect: 'Home',
 			header: 'Beth Masterson and Andrew Knapp',
 			backImg: 'url(..//Beth/img/flowers.jpeg)'
 		},
 		{
+			sect: 'Details',
 			header: 'Ceremony and Reception Details',
 			backImg: 'url(..//Beth/img/bride.jpg)'
 		},
 		{
-
+			sect: 'Hotels',
 			header: 'Hotel Information',
 			backImg: 'url(..//Beth/img/hotel.jpeg)'
 		}	
@@ -36,20 +38,36 @@ var model = {
 var viewModel = {
 	init: function() {
 		picSwap();
+		slider.init();
 	}
 };
 
-		function picSwap () {
-		  $( ".left" ).animate({
-		    opacity: 0.75,
-		    height: "toggle"
-		  }, 1400, function() {
-		  	$(".left").fadeOut(1000);
-		  	$(".right").fadeOut(1000);
-		  	$(".both").fadeIn(1200); 
-		  })
+function picSwap () {
+  $( ".left" ).animate({
+    opacity: 0.75,
+    height: "toggle"
+  }, 1400, function() {
+  	$(".left").fadeOut(1000);
+  	$(".right").fadeOut(1000);
+  	$(".both").fadeIn(1200); 
+  })
 
-		};
+};
+
+/**** slider credit goes to kentowatanabe, found at: 
+ 	[http://www.jqueryscript.net/demo/Simplest-Fullscreen
+ 		-Background-Slideshow-With-jQuery-Chocolate-js/]
+ ****/
+var slider = {
+	
+	init: function() {
+		$('.head-div').chocolate({
+			images    : ['img/both.jpg', 'img/both-two.jpg', 'img/both-four.jpg'],
+			interval : 3500,
+			speed: 1000
+		});
+	}
+};
 
 ko.applyBindings(viewModel.init());
 
