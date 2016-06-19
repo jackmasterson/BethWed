3ffbd76b43e8c1c023d4a01d2d7689013744d911
JackMasterson <jackmasterson5@gmail.com>
2016-06-19 14:27:28 -0400

    Add switching animation

--- js/script.js
+++ js/script.js
@@ -1,6 +1,24 @@
 'use strict';
 
 var model = {
+	navHead: [
+		{
+			headers: "Home",
+			divID: "home"
+		},
+		{ 
+			headers: "Wedding",
+			divID: "info"
+		}, 
+		{
+			headers: "Accommodations",
+			divID: "accommodations"
+		},
+		{
+			headers: "Things to Do",
+			divID: "things"
+		}
+	],
 	weddingInfo: [
 		{
 			header: 'The Wedding',
@@ -9,13 +27,13 @@ var model = {
 			directions: ko.observable(false)
 		},
 		{
-			header: "The Church",
+			header: "The Ceremony",
 			location: "St. Mark's Catholic Church",
 			address: "215 Crescent Parkway, Sea Girt, NJ 08750",
 			directions: ko.observable(false)
 		},
 		{
-			header: "The Ceremony",
+			header: "The Reception",
 			location: "TBD",
 			address: "514 Sunset Drive, Downingtown, PA",
 			directions: {
@@ -30,6 +48,65 @@ var model = {
 				}
 		}
 	],
+	accommodationsInfo: [
+		{
+			header: 'Airports',
+			location: 'JFK',
+			address: '2:00pm',
+			directions: ko.observable(false)
+		},
+		{
+			header: "LGA",
+			location: "ERrtahng",
+			address: "215 Crescent Parkway, Sea Girt, NJ 08750",
+			directions: ko.observable(false)
+		},
+		{
+			header: "Accommodations",
+			location: "TBD",
+			address: "514 Sunset Drive, Downingtown, PA",
+			directions: {
+				text: "Directions",
+				href: "https://www.google.com/maps/dir/"+
+					"/Crystal+Point+Inn,+3900+River+Rd,+Point+Pleasant,+NJ"+
+					"+08742/@40.0954926,-74.0891644,17z/data=!4m16!1m7!3m6!1"+
+					"s0x89c18682b7849b41:0x9fe4461d1758c521!2sCrystal+Point+Inn!"+
+					"3b1!8m2!3d40.0954926!4d-74.0869757!4m7!1m0!1m5!1m1!1s0x"+
+					"89c18682b7849b41:0x9fe4461d1758c521!2m2!1d-74.0869757"+
+					"!2d40.0954926"
+				}
+		}
+	],
+	thingsInfo: [
+		{
+			header: 'Things',
+			location: 'December 31, 2016',
+			address: '2:00pm',
+			directions: ko.observable(false)
+		},
+		{
+			header: "Things Info",
+			location: "St. Mark's Catholic Church",
+			address: "215 Crescent Parkway, Sea Girt, NJ 08750",
+			directions: ko.observable(false)
+		},
+		{
+			header: "More Things Info",
+			location: "TBD",
+			address: "514 Sunset Drive, Downingtown, PA",
+			directions: {
+				text: "Directions",
+				href: "https://www.google.com/maps/dir/"+
+					"/Crystal+Point+Inn,+3900+River+Rd,+Point+Pleasant,+NJ"+
+					"+08742/@40.0954926,-74.0891644,17z/data=!4m16!1m7!3m6!1"+
+					"s0x89c18682b7849b41:0x9fe4461d1758c521!2sCrystal+Point+Inn!"+
+					"3b1!8m2!3d40.0954926!4d-74.0869757!4m7!1m0!1m5!1m1!1s0x"+
+					"89c18682b7849b41:0x9fe4461d1758c521!2m2!1d-74.0869757"+
+					"!2d40.0954926"
+				}
+		}
+	],
+
 	mainSect: [
 	/*	{
 			sect: 'Home',
@@ -115,13 +192,14 @@ var counter = {
 		var count = document.getElementsByClassName('counter')[0];
 		var bigDay = new Date(2016, 11, 31);
 		var today = new Date();
+		document.write((today.getTime())/1000);
+		document.write((bigDay.getTime())/1000);
 		var daysLeft_ms = bigDay - today;
 		var one_day = 1000*60*60*24;
 		var daysLeft = Math.round(daysLeft_ms/one_day);
 
-		document.write((today.getTime())/1000);
-		document.write((bigDay.getTime())/1000);
 
+		console.log(daysLeft);
 		count.innerHTML = daysLeft;
 
 
@@ -149,14 +227,15 @@ var counter = {
 
 var toggle = {
 	nav: function(clicked) {
-		var classed = "#" + clicked.divID;
+		var classed = "." + clicked.divID;
 		console.log(clicked);
-		$('.main').hide();
+		console.log(classed);
+		$('.all').hide();
 		$(classed).fadeIn('slow', function(){});
 
-		$('html, body').animate({
-	        scrollTop: $(classed).offset().top
-	    }, 800);
+	//	$('html, body').animate({
+	  //      scrollTop: $(classed).offset().top
+	    //}, 800);
 	},
 
 	top: function(clicked) {
